dbdd485a097a992c810309b9e82fd9ca8a42b92d
Miroredge <Miroredge@users.noreply.github.com>
2020-09-26 20:48:03 +0200

    Revert "Modifs code"
    This reverts commit 41f163f4b0e89be641b3c4ec1375d58c8e7bb501.

--- raquette.py
+++ raquette.py
@@ -11,30 +11,23 @@ class Raquette :
         '''
 
         self.jeu = jeu
-        self.x = (jeu.largeur_fenetre) / 2
-        self.y = (jeu.hauteur_fenetre) / 8
+        self.x = (self.jeu(largeur_fenetre)) / 2
+        self.y = (self.jeu(hauteur_fenetre)) / 8
         self.largeur = 45
         self.hauteur = 20
         self.vitesse = 15
-        self.image = self._dessiner()
+        self.image = self.jeu.canvas.
 
-    def _dessiner (self) :
-        
-        return self.jeu.canvas.create_rectangle(self.x, self.y, self.x + self.largeur, self.y + self.hauteur, fill = "red")
 
 
     def aller_Droite (self) :
 
-        pass
-
+        
 
     def aller_Gauche (self) :
 
-        pass
 
-    
     def collision (self, x) :
 
-        pass
 
 
