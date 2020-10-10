45cc4813b016c5e3fc92b91aee090a2d9a11268b
Corentin-Skyspirit <corentin.arnaud45@gmail.com>
2020-10-10 15:49:33 +0200

    evolution de la fonction deplacement + fix de quelques bugs

--- balle.py
+++ balle.py
@@ -12,7 +12,7 @@ class Balle:
         self.y = 200 #Position y du coin supérieur gauche de la balle
         self.rayon = 10 #Taille de la balle
         self.vitesse_x = 0 #Vitesse sur l'axe x
-        self.vitesse_y = -1 #Vitesse sur l'axe y
+        self.vitesse_y = 2 #Vitesse sur l'axe y
         self.objet = 0 #initialisation de la variable local objet
 
     def dessiner(self): #Créer la balle dans le canevas
@@ -22,34 +22,37 @@ class Balle:
 
     def deplacer(self): #Bouger la balle dans le canevas pour la raquette
         #-----------------------------Raquette-----------------------------#
-        if self.jeu.raquette.collision(self.x+self.rayon) == 1: #Si la collision revoie 1
+        if self.jeu.raquette.collision(self.x+self.rayon) == 1 and self.y-self.rayon*2 >= 485 and self.y-self.rayon*2 <= 505 : #Si la collision revoie 1
             self.vitesse_x = -1
             self.vitesse_y = -2
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
-        elif self.jeu.raquette.collision(self.x+self.rayon) == 2: #Si la collision revoie 2
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        elif self.jeu.raquette.collision(self.x+self.rayon) == 2 and self.y-self.rayon*2 >= 485 and self.y-self.rayon*2 <= 505 : #Si la collision revoie 2
             self.vitesse_x = -2
             self.vitesse_y = -1
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
-        elif self.jeu.raquette.collision(self.x+self.rayon) == 3: #Si la collision revoie 3
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        elif self.jeu.raquette.collision(self.x+self.rayon) == 3 and self.y-self.rayon*2 >= 485 and self.y-self.rayon*2 <= 505 : #Si la collision revoie 3
             self.vitesse_x = -2
             self.vitesse_y = 0
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
-        elif self.jeu.raquette.collision(self.x+self.rayon) == 4: #Si la collision revoie 4
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        elif self.jeu.raquette.collision(self.x+self.rayon) == 4 and self.y-self.rayon*2 >= 485 and self.y-self.rayon*2 <= 505 : #Si la collision revoie 4
             self.vitesse_x = -2
             self.vitesse_y = 1
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
-        elif self.jeu.raquette.collision(self.x+self.rayon) == 5: #Si la collision revoie 5
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        elif self.jeu.raquette.collision(self.x+self.rayon) == 5 and self.y-self.rayon*2 >= 485 and self.y-self.rayon*2 <= 505 : #Si la collision revoie 5
             self.vitesse_x = -1
             self.vitesse_y = 2
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
         #-----------------------------Bords de la fenêtre-----------------------------#
-        elif self.jeu.hauteur_fenetre == self.y + self.rayon*2 or self.y == 0 : #Si la balle touche le haut ou le bas, ça inverse la vitesse en ordonnée
+        elif self.jeu.hauteur_fenetre <= self.y + self.rayon*2 or self.y <= 0 : #Si la balle touche le haut ou le bas, ça inverse la vitesse en ordonnée
             self.vitesse_y = -(self.vitesse_y)
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
-        elif self.jeu.largeur_fenetre == self.x + self.rayon*2 or self.x == 0 : #Si la balle touche la gauche ou la droite, ça inverse la vitesse en abscisse
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        elif self.jeu.largeur_fenetre <= self.x + self.rayon*2 or self.x <= 0 : #Si la balle touche la droite ou la gauche, ça inverse la vitesse en abscisse
             self.vitesse_x = -(self.vitesse_x)
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
         #-----------------------------Sinon suit la trajectoire-----------------------------#
         else:
-            self.jeu.canevas.move(self.jeu.balle,self.vitesse_x,self.vitesse_y)
+            self.jeu.canevas.move(self.objet,self.vitesse_x,self.vitesse_y)
+        self.x += self.vitesse_x
+        self.y += self.vitesse_y
+        # print(self.x,self.y,self.x + self.rayon*2,self.y-self.rayon*2,self.jeu.raquette.collision(self.x+self.rayon))
         return None
